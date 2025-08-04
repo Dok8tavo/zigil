d82e7b7e2699f6c37924f0dc94e4cf834dd3fa1f
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2025-08-04 19:00:24 +0200

    different naming convention for fails

--- fail/trait/is_optional.zig
+++ fail/trait/is_optional.zig
@@ -3,5 +3,5 @@ const z = @import("zigil");
 
 const isOptional = z.Trait.isOptional;
 
-pub const wrong_kind = if (!root.is_listing) isOptional(.no_trait).assert(u32);
-pub const wrong_child = if (!root.is_listing) isOptional(.is(i32)).assert(?u32);
+pub const kind = if (!root.is_listing) isOptional(.no_trait).assert(u32);
+pub const child = if (!root.is_listing) isOptional(.is(i32)).assert(?u32);
--- fail/trait/is_vector.zig
+++ fail/trait/is_vector.zig
@@ -3,5 +3,5 @@ const root = @import("../root.zig");
 
 const isVector = z.Trait.isVector;
 
-pub const wrong_kind = if (!root.is_listing) isVector(.{}).assert(i32);
-pub const wrong_child = if (!root.is_listing) isVector(.{ .child = .isKind(.float) }).assert(@Vector(8, i32));
+pub const kind = if (!root.is_listing) isVector(.{}).assert(i32);
+pub const child = if (!root.is_listing) isVector(.{ .child = .isKind(.float) }).assert(@Vector(8, i32));
